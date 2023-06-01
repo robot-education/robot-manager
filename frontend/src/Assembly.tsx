7db59725f8cac362d99767866495cc071084c12c
Alex Kempen <alex.bookreader@gmail.com>
2023-06-01 14:31:55 +0000

    Cleaned out src

---
+++ frontend/src/Assembly.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import material from "@mui/material";
+
+function AssemblyApp(): JSX.Element {
+    return (
+        <material.AppBar position="fixed">
+            <material.Toolbar>Auto assembly</material.Toolbar>
+        </material.AppBar>
+    );
+}
